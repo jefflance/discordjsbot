e1273396d4bb4470f07a0e5e0c8a3a3e3d08b1d5
Jeff LANCE <jeff.lance@mala.fr>
2020-03-16 18:44:27 +0100

    Add first files

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "${MODIFIED_STARTUP}"
+
+# Run the Server
+npm install --production --quiet --no-progress --loglevel=error && npm audit fix --quiet --no-progress --loglevel=error
+eval ${MODIFIED_STARTUP}
